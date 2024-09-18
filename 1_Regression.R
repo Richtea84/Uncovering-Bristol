e67e9eaa312b49f9d5148f2b2ff4b40722acfe08
Richard Timmerman <38284042+Richtea84@users.noreply.github.com>
2024-09-18 15:56:41 +0100

    Add files via upload

---
+++ 1_Regression.R
@@ -0,0 +1,88 @@
+# Regression modelling
+
+# Usual suspects
+if(!require(jquerylib)) install.packages("jquerylib")
+if(!require(proxy)) install.packages("proxy")
+if(!require(labelled)) install.packages("labelled")
+if(!require(terra)) install.packages("terra")
+
+# Packages
+if(!require(tidyverse)) install.packages("tidyverse") &
+  require(tidyverse)
+
+if(!require(randomcoloR)) install.packages("randomcoloR") &
+  require(randomcoloR)
+
+# Data and processing
+data_in <- read.csv(
+  list.files(path = ".", pattern = "_Survey"
+             )
+)
+
+data_in |>
+  rename(
+    "Stop 1" = "Stop.1",
+    "Stop 2" = "Stop.2",
+    "Stop 3" = "Stop.3",
+    "Stop 4" = "Stop.4",
+    "Stop 5" = "Stop.5",
+    "Stop 6" = "Stop.6",
+    "Stop 7" = "Stop.7",
+    "Stop 8" = "Stop.8",
+    "Stop 9" = "Stop.9",
+    "Stop 10" = "Stop.10"
+  ) -> data_out
+
+# Unweighted scores
+colMeans(x = data_out[,-c(1,2)]) -> Unweighted_average
+
+# Weighted scores 
+apply(
+  X = data_out[,-c(1,2)], MARGIN = 2,
+  FUN = function(x) weighted.mean(
+    x = as.numeric(x[1:8]), w = data_out$Weights
+    )
+  ) -> Weighted_average
+
+# IMD data
+
+IMD_scores <- read.csv(
+  list.files(path = ".", pattern = "IMD")
+)
+
+IMD_scores_agg <- aggregate(Soc_grad ~ Stop.no., data = IMD_scores, FUN = mean)
+
+# Regression model & Pearson's correlation coefficient
+
+my_pal = randomcoloR::randomColor(count = 10)
+
+windowsFonts(A = windowsFont("Arial"))
+
+jpeg("Weighted Regression Plot.jpg", height = 720, width = 1080)
+plot(IMD_scores_agg$Soc_grad ~ Weighted_average, family = "A",
+     xlab = expression(paste("weighted ", bar(x))),
+     ylab = "Index of multiple\ndeprivation ranking")
+abline(lm(IMD_scores_agg$Soc_grad ~ Weighted_average), col = my_pal)
+dev.off()
+
+jpeg("Unweighted Regression Plot.jpg", height = 720, width = 1080)
+plot(IMD_scores_agg$Soc_grad ~ Unweighted_average, family = "A",
+     xlab = expression(paste("unweighted ", bar(x))),
+     ylab = "Index of multiple\ndeprivation ranking")
+abline(lm(IMD_scores_agg$Soc_grad ~ Weighted_average), col = my_pal)
+dev.off()
+
+
+cor.test(x = Weighted_average, y = IMD_scores_agg$Soc_grad)
+cor.test(x = Unweighted_average, y = IMD_scores_agg$Soc_grad)
+
+# Unique Identifier
+UI <- ifelse("E00174307" %in% IMD_scores$name, "Route_1",
+             ifelse("E00073150" %in% IMD_scores$name, "Route_2",
+                    ifelse("E00073179" %in% IMD_scores$name, "Route_3",
+                           ifelse("E00073653" %in% IMD_scores$name, "Route_4")
+                           )
+                    )
+             )
+
+
---
+++ 2_Mapping.R
@@ -0,0 +1,99 @@
+if(!require(sf)) install.packages("sf") &
+  require(sf)
+if(!require(tmap)) install.packages("tmap") &
+  require(tmap)
+if(!require(rosm)) install.packages("rosm") &
+  require(rosm)
+if(!require(viridis)) install.packages("viridis") &
+  require(viridis)
+
+source("1_Regression.R")
+
+# Map data
+Route_poly <- if(UI == "Route_1") read_sf(dsn = "https://raw.githubusercontent.com/Richtea84/Uncovering-Bristol/main/route_1_2023.kml") else 
+  if(UI == "Route_2") read_sf(dsn = "https://raw.githubusercontent.com/Richtea84/Uncovering-Bristol/main/route_2_2023.kml") else
+    if(UI == "Route_3") read_sf(dsn = "https://raw.githubusercontent.com/Richtea84/Uncovering-Bristol/main/route_3_2023.kml") else
+      if(UI == "Route_4") read_sf(dsn = "https://raw.githubusercontent.com/Richtea84/Uncovering-Bristol/main/route_4_2023.kml") else NA
+
+Route_poly_a <- merge(x = Route_poly, y = IMD_scores, by.x = "Name", by.y = "name")
+Route_poly_a$Stop.no. <- as.factor(Route_poly_a$Stop.no.)
+
+Weighted_average <- as.data.frame(Weighted_average)
+Unweighted_average <- as.data.frame(Unweighted_average)
+
+ifelse(Route_poly_a$Stop.no. == "1", Weighted_average$Weighted_average[1],
+       ifelse(Route_poly_a$Stop.no. == "2", Weighted_average$Weighted_average[2],
+              ifelse(Route_poly_a$Stop.no. == "3", Weighted_average$Weighted_average[3],
+                      ifelse(Route_poly_a$Stop.no. == "4", Weighted_average$Weighted_average[4],
+                             ifelse(Route_poly_a$Stop.no. == "5", Weighted_average$Weighted_average[5],
+                                    ifelse(Route_poly_a$Stop.no. == "6", Weighted_average$Weighted_average[6],
+                                           ifelse(Route_poly_a$Stop.no. == "7", Weighted_average$Weighted_average[7],
+                                                  ifelse(Route_poly_a$Stop.no. == "8", Weighted_average$Weighted_average[8],
+                                                         ifelse(Route_poly_a$Stop.no. == "9", Weighted_average$Weighted_average[9],
+                                                                ifelse(Route_poly_a$Stop.no. == "10", Weighted_average$Weighted_average[10], NA)))))))))) -> Route_poly_a$My_Weighted_Scores
+       
+
+ifelse(Route_poly_a$Stop.no. == "1", Unweighted_average$Unweighted_average[1],
+       ifelse(Route_poly_a$Stop.no. == "2", Unweighted_average$Unweighted_average[2],
+              ifelse(Route_poly_a$Stop.no. == "3", Unweighted_average$Unweighted_average[3],
+                     ifelse(Route_poly_a$Stop.no. == "4", Unweighted_average$Unweighted_average[4],
+                            ifelse(Route_poly_a$Stop.no. == "5", Unweighted_average$Unweighted_average[5],
+                                   ifelse(Route_poly_a$Stop.no. == "6", Unweighted_average$Unweighted_average[6],
+                                          ifelse(Route_poly_a$Stop.no. == "7", Unweighted_average$Unweighted_average[7],
+                                                 ifelse(Route_poly_a$Stop.no. == "8", Unweighted_average$Unweighted_average[8],
+                                                        ifelse(Route_poly_a$Stop.no. == "9", Unweighted_average$Unweighted_average[9],
+                                                               ifelse(Route_poly_a$Stop.no. == "10", Unweighted_average$Unweighted_average[10], NA)))))))))) -> Route_poly_a$My_Unweighted_Scores
+
+
+# Visual output
+Route_poly_b <- st_transform(Route_poly_a, 4326)
+
+base_map <- rosm::osm.raster(Route_poly_b)
+
+# Social grade mapping...
+Map_1 <- tm_shape(base_map)+
+  tm_rgb(saturation = 0)+
+  tm_shape(Route_poly_b)+
+  tm_polygons("Soc_grad", palette = "RdBu", n = 5,
+              title = "No. people in\nSemi & Unskilled Occupations", alpha = 0.6)+
+  tm_compass()+
+  tm_scale_bar()+
+  tm_layout(legend.outside = T,main.title = "Deprivation based on social grade",
+            main.title.position = c("left", "top"))
+
+# Unweighted deprivation
+Map_2 <- tm_shape(base_map)+
+  tm_rgb(saturation = 0)+
+  tm_shape(Route_poly_b)+
+  tm_polygons("My_Unweighted_Scores", palette = "BrBG", n = 5,
+              title = expression(paste("Unweighted ", bar(x), " values")), alpha = 0.6)+
+  tm_compass()+
+  tm_scale_bar()+
+  tm_layout(legend.outside = T,main.title = "Unweighted environmental\nquality scores",
+            main.title.position = c("left", "top"))
+
+# Weighted deprivation
+Map_3 <- tm_shape(base_map)+
+  tm_rgb(saturation = 0)+
+  tm_shape(Route_poly_b)+
+  tm_polygons("My_Weighted_Scores", palette = "BrBG", n = 5,
+              title = expression(paste("Unweighted ", bar(x), " values")), alpha = 0.6)+
+  tm_compass()+
+  tm_scale_bar()+
+  tm_layout(legend.outside = T,main.title = "Weighted environmental\nquality scores",
+            main.title.position = c("left", "top"))
+
+
+jpeg(filename = "Social Grade Map.jpg", height = 1920, width = 1080, quality = 600)
+Map_1
+dev.off()
+
+jpeg(filename = "Unweighted Scores Map.jpg", height = 1920, width = 1080, quality = 600)
+Map_2
+dev.off()
+
+jpeg(filename = "Weighted Scorese Map.jpg", height = 1920, width = 1080, quality = 600)
+Map_3
+dev.off()
+
+  
